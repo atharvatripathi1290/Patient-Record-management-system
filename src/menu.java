e2f3498f2e507c52f51a5b32af70442296a9f167
atharvatripathi1290 <atharvatrip1290@gmail.com>
2025-11-24 16:33:26 +0530

    Create menu.java

---
+++ src/menu.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Menu {
+
+    public static void showMenu() {
+        System.out.println("\n====== HEALTHCARE SYSTEM ======");
+        System.out.println("1. Add Patient");
+        System.out.println("2. Search Patient");
+        System.out.println("3. View All Patients");
+        System.out.println("4. Exit");
+        System.out.print("Enter choice: ");
+    }
+}
